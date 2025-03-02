943d9265cbb43be87dcc497634dcc311b09f6861
Uday Goel <udaygoel295@gmail.com>
2025-03-02 21:11:27 +0530

    added gitignore

--- client/src/components/JobCard.jsx
+++ client/src/components/JobCard.jsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const JobCard = ({ job }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="border p-6 shadow rounded">
       <div className="flex justify-between items-center">
@@ -21,10 +24,22 @@ const JobCard = ({ job }) => {
         dangerouslySetInnerHTML={{ __html: job.description.slice(0, 150) }}
       ></p>
       <div className="mt-4 flex gap-4 text-sm">
-        <button className="bg-blue-600 text-white px-4 py-2 rounded">
+        <button
+          onClick={() => {
+            navigate(`/apply-job/${job._id}`);
+            scrollTo(0, 0);
+          }}
+          className="bg-blue-600 text-white px-4 py-2 rounded"
+        >
           Apply Now
         </button>
-        <button className="text-gray-500 border border-gray-500 rounded px-4 py-2">
+        <button
+          onClick={() => {
+            navigate(`/apply-job/${job._id}`);
+            scrollTo(0, 0);
+          }}
+          className="text-gray-500 border border-gray-500 rounded px-4 py-2"
+        >
           Learn more
         </button>
       </div>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,6 +13,8 @@ export const AppContextProvider = (props) => {
 
   const [jobs, setJobs] = useState([]);
 
+  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
+
   //function to fetch jobs
   const fetchJobs = async () => {
     setJobs(jobsData);
@@ -29,6 +31,8 @@ export const AppContextProvider = (props) => {
     setIsSearched,
     jobs,
     setJobs,
+    showRecruiterLogin,
+    setShowRecruiterLogin,
   };
 
   return (

733835ea1e7e1171ebc98c409d716dbaac9a7b0c
Uday Goel <udaygoel295@gmail.com>
2025-03-03 22:39:27 +0530

    frontend done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,11 @@ import ApplyJob from "./pages/ApplyJob";
 import Applications from "./pages/Applications";
 import RecruiterLogin from "./components/RecruiterLogin";
 import { AppContext } from "./context/AppContext";
+import Dashboard from "./pages/Dashboard";
+import AddJob from "./pages/AddJob";
+import ManageJobs from "./pages/ManageJobs";
+import ViewApplications from "./pages/ViewApplications";
+import "quill/dist/quill.snow.css";
 
 const App = () => {
   const { showRecruiterLogin } = useContext(AppContext);
@@ -16,6 +21,11 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/apply-job/:id" element={<ApplyJob />} />
         <Route path="/applications" element={<Applications />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="manage-jobs" element={<ManageJobs />} />
+          <Route path="view-applications" element={<ViewApplications />} />
+        </Route>
       </Routes>
     </div>
   );

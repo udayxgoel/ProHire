d93d66f72d97eff3eb9b0ad063ae31f3b1a561c3
Uday Goel <udaygoel295@gmail.com>
2025-03-05 18:57:21 +0530

    webhook changes

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -1,24 +1,23 @@
 import { Webhook } from "svix";
 import { User } from "../models/user.model.js";
 
-// API Controller Function to Manage Clerk User with database
+//API Controller Function to Manage Clerk User with database
 export const clerkWebHooks = async (req, res) => {
   try {
-    console.log("🔹 Webhook Received: ", JSON.stringify(req.body, null, 2));
-    // Create a Svix instance with clerk webhook secret
+    //Create a Svix instance with clerk webhook secret
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
-    // Verifying Headers
+    //Verifying Headers
     await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
     });
 
-    // Getting data from req.body
+    //Getting Data from request body
     const { data, type } = req.body;
 
-    // Switch case for diffrent Events
+    //Switch case for different Events
     switch (type) {
       case "user.created": {
         const userData = {

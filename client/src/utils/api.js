da3a0e11f8a387ebe685b263ae8a613d2c3faafc
Uday Goel <udaygoel295@gmail.com>
2025-06-15 21:53:32 +0530

    api changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,8 +1,7 @@
 export const USER_API_END_POINT =
-  "https://prohire-backend.vercel.app/api/v1/user";
-export const JOB_API_END_POINT =
-  "https://prohire-backend.vercel.app/api/v1/job";
+  "https://prohire-0csr.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://prohire-0csr.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://prohire-backend.vercel.app/api/v1/application";
+  "https://prohire-0csr.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://prohire-backend.vercel.app/api/v1/company";
+  "https://prohire-0csr.onrender.com/api/v1/company";

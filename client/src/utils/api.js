db6a00a98691c004f598b13518f5c7daeaa80201
Uday Goel <udaygoel295@gmail.com>
2025-06-15 17:18:06 +0530

    frontend api changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,5 +1,8 @@
-export const USER_API_END_POINT = "http://localhost:4000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:4000/api/v1/job";
+export const USER_API_END_POINT =
+  "https://prohire-backend.vercel.app/api/v1/user";
+export const JOB_API_END_POINT =
+  "https://prohire-backend.vercel.app/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "http://localhost:4000/api/v1/application";
-export const COMPANY_API_END_POINT = "http://localhost:4000/api/v1/company";
+  "https://prohire-backend.vercel.app/api/v1/application";
+export const COMPANY_API_END_POINT =
+  "https://prohire-backend.vercel.app/api/v1/company";

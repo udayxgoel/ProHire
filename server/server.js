2f452a150496feffe8909aca7df9905ff5571350
Uday Goel <udaygoel295@gmail.com>
2025-06-16 10:16:38 +0530

    server.js changes

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ import ApplicationRoute from "./routes/application.route.js";
 dotenv.config({});
 
 const app = express();
+const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,16 +21,14 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 4000;
-
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server is running on port ${PORT}`);
-});
-
 // Routes
 app.get("/", (req, res) => res.send("API Working"));
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", ApplicationRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
